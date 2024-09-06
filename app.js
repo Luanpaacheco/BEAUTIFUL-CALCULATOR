5bdf63d6e6758a2d1c34b9389e2d5bef487f43b5
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-09-06 13:52:23 -0300

    ultimo detalhes

--- app.js
+++ app.js
@@ -2,139 +2,122 @@ let numero1;
 let numero2;
 let primeira = true;
 let operador;
-let historico;
+let historico = "";
 
-function colocar(N){
-    if(primeira){
-        var numero=document.getElementById("resultado").innerHTML;
-        document.getElementById("resultado").innerHTML=numero+N;
-        document.getElementById("historico").innerHTML=numero+N; 
-    }else{
-        var numerox=document.getElementById("resultado").innerHTML;
-        document.getElementById("resultado").innerHTML=numerox+N;
-        var numero=document.getElementById("historico").innerHTML;
-        historico=numero;
-        document.getElementById("historico").innerHTML=numero+N;
+function colocar(N) {
+    let resultadoElement = document.getElementById("resultado");
+    let historicoElement = document.getElementById("historico");
 
+    if (primeira) {
+        let numero = resultadoElement.innerHTML;
+        resultadoElement.innerHTML = numero + N;
+        historico = historico + N;
+        historicoElement.innerHTML = historico;
+    } else {
+        let numerox = resultadoElement.innerHTML;
+        resultadoElement.innerHTML = numerox + N;
+        historico += N;
+        historicoElement.innerHTML = historico;
     }
-    
-
-    
-    
-}
-function apagarTudo(){
-    document.getElementById("resultado").innerHTML="";
-    document.getElementById("historico").innerHTML="";
-    primeira=true;
-    numero1=0;
-    numero2=0;
-    historico="";
-    
-}
-function apagarCE(){
-    document.getElementById("historico").innerHTML=historico;
-    document.getElementById("resultado").innerHTML="";
 }
 
-function apagar(){
-    //document.getElementById("historico").innerHTM=document.getElementById("historico").innerHTM.substring(document.getElementById("resultado").innerHTML);
-    document.getElementById("resultado").innerHTML="";
-
-    // primeira=true;
-    
-    
+function apagarTudo() {
+    document.getElementById("resultado").innerHTML = "";
+    document.getElementById("historico").innerHTML = "";
+    primeira = true;
+    numero1 = 0;
+    numero2 = 0;
+    historico = "";
 }
 
+// function apagarCE() {
+//     document.getElementById("resultado").innerHTML = "";
+    
+// }
 
-function opera(operado){
-    var numero=document.getElementById("historico").innerHTML;
-    document.getElementById("historico").innerHTML= numero+operado;
-    if(primeira){
-        operador=operado;
-        numero1=Number(document.getElementById("resultado").innerHTML);
+function apagar() {
+    document.getElementById("resultado").innerHTML = "";
+}
+
+function opera(operado) {
+    let resultadoElement = document.getElementById("resultado");
+    let numero = resultadoElement.innerHTML;
+
+    if (operado === "@") {
+        // Inverter o sinal do último número digitado
+        let partes = historico.split(/[\+\-\*\/\%\s]/);
+        let ultimoNumero = partes[partes.length - 1];
+        if (ultimoNumero) {
+            let valorInvertido = Number(ultimoNumero) * -1;
+            resultadoElement.innerHTML = valorInvertido;
+            historico = historico.slice(0, historico.lastIndexOf(ultimoNumero)) + valorInvertido;
+            document.getElementById("historico").innerHTML = historico;
+        }
+        return;
+    }
+
+    if (primeira) {
+        operador = operado;
+        numero1 = Number(resultadoElement.innerHTML);
         apagar();
+        historico += " " + operador + " ";
         if(operado=='%'){
             operador=operado;
             mostrar();
         }
-        if(operado=='@'){
-            document.getElementById("resultado").innerHTML=Number(document.getElementById("resultado").innerHTML)*(-1);
-        }
-        primeira=false;
-    }else {
+    } else {
         switch (operador) {
             case '+':
-                numero1=(Number(numero1)+Number(document.getElementById("resultado").innerHTML));
-                
+                numero1 += Number(resultadoElement.innerHTML);
                 break;
             case '*':
-                numero1=numero1*Number(document.getElementById("resultado").innerHTML);
-                
+                numero1 *= Number(resultadoElement.innerHTML);
                 break;
             case '-':
-                numero1=numero1-Number(document.getElementById("resultado").innerHTML);
-                    
+                numero1 -= Number(resultadoElement.innerHTML);
                 break;
             case '/':
-                numero1=numero1/Number(document.getElementById("resultado").innerHTML);
-                
-                break;
-            case '*':
-                numero1=numero1*Number(document.getElementById("resultado").innerHTML);
-                
+                numero1 /= Number(resultadoElement.innerHTML);
                 break;
             case '%':
-                    operador=operado;
-                    mostrar();
-                    break;
-            case '@':
-                Number(document.getElementById("resultado").innerHTML)=Number(document.getElementById("resultado").innerHTML)*(-1);
-                        break;
+                operador=operado;
+                mostrar();
+                break;
+                
             default:
                 break;
         }
-        operador=operado;
+        operador = operado;
         apagar();
-    }    
+        historico += " " + operador + " ";
+    }
+    primeira = false;
 }
 
-
-
-function mostrar(){
-    numero2=Number(document.getElementById("resultado").innerHTML);
-    var resultado;
+function mostrar() {
+    numero2 = Number(document.getElementById("resultado").innerHTML);
+    let resultado;
     switch (operador) {
         case '+':
-            resultado=(Number(numero1)+Number(numero2));
-            document.getElementById("resultado").innerHTML=resultado;
+            resultado = numero1 + numero2;
             break;
         case '*':
-            resultado=numero1*numero2;
-            document.getElementById("resultado").innerHTML=resultado;
+            resultado = numero1 * numero2;
             break;
         case '-':
-            resultado=numero1-numero2;
-            document.getElementById("resultado").innerHTML=resultado;
+            resultado = numero1 - numero2;
             break;
         case '/':
-            resultado=numero1/numero2;
-            document.getElementById("resultado").innerHTML=resultado;
-            break;
-        case '*':
-            resultado=numero1*numero2;
-            document.getElementById("resultado").innerHTML=resultado;
+            resultado = numero1 / numero2;
             break;
         case '%':
-            resultado=numero1/100;
-            document.getElementById("resultado").innerHTML=resultado;
+            resultado = numero1 / 100;
             break;
         default:
             break;
     }
-    primeira=true;
-    
+    document.getElementById("resultado").innerHTML = resultado;
+    historico += " = " + resultado;
+    document.getElementById("historico").innerHTML = historico;
+    primeira = true;
 }
-
-
-
-    
